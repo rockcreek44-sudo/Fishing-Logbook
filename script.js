02ff652647ef9b58c0c7ce02b4349851b4d961b6
rockcreek44-sudo <rockcreek44@gmail.com>
2026-06-30 20:08:24 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("newTripBtn").addEventListener("click", () => {
+  alert("New Trip - Coming in Milestone 2");
+});
+
+document.getElementById("viewTripsBtn").addEventListener("click", () => {
+  alert("View Trips - Coming in Milestone 2");
+});
